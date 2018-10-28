35f4ddec1d0cab556537a85d1419840eee9264f7
Ptit-Biscuit <vincent.brebion@etu.univ-nantes.fr>
2018-10-28 12:14:46 +0100

    Add example scene
    How to use WWW in unity (API test)

---
+++ Assets/ApiManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ApiManager : MonoBehaviour {
+
+	public Text text;
+	
+	public void Reset()
+	{
+		StartCoroutine(getReset());
+	}
+
+	IEnumerator getReset()
+	{
+		using (WWW reset = new WWW("http://51.15.121.74:3000/reset"))
+		{
+			yield return reset;
+			text.text = reset.text;
+		}
+	}
+
+	public void Coucou()
+	{
+		StartCoroutine(getCoucou());
+	}
+
+	IEnumerator getCoucou()
+	{
+		using (WWW coucou = new WWW("http://51.15.121.74:3000/coucou"))
+		{
+			yield return coucou;
+			text.text = coucou.text;
+		}
+	}
+
+	public void Bonjour()
+	{
+		StartCoroutine(getBonjour());
+	}
+
+	IEnumerator getBonjour()
+	{
+		using (WWW bonjour = new WWW("http://51.15.121.74:3000/bonjour"))
+		{
+			yield return bonjour;
+			text.text = bonjour.text;
+		}
+	}
+}
